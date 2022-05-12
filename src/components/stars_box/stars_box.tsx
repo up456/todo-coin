080138574ecd2bcf00f739c00d0a145c88a3aadb
Hihyu <4up456@naver.com>
2022-05-12 17:23:15 +0900

    stars_box에 key값 추가

--- src/components/stars_box/stars_box.tsx
+++ src/components/stars_box/stars_box.tsx
@@ -4,11 +4,18 @@ import React from 'react';
 const StarsBox = ({ satisfaction }: { satisfaction: number }) => {
   return (
     <div className={styles.starsBox}>
-      {[...Array(satisfaction)].map(() => (
-        <img className={styles.onStar} src="asset/star.png" alt="별"></img>
+      {[...Array(satisfaction)].map((_, idx) => (
+        <img
+          key={idx}
+          className={styles.onStar}
+          src="asset/star.png"
+          alt="별"
+        ></img>
       ))}
-      {[...Array(3 - satisfaction)].map(() => (
-        <span className={styles.offStar}>★</span>
+      {[...Array(3 - satisfaction)].map((_, idx) => (
+        <span key={idx} className={styles.offStar}>
+          ★
+        </span>
       ))}
     </div>
   );
