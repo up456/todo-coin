7e3d51f8c46b71a126d3b285b26db2de36bac3ba
Hihyu <4up456@naver.com>
2022-05-13 20:35:31 +0900

    slectbox컴포넌트: 틀만 완성

---
+++ src/components/select_box/select_box.tsx
@@ -0,0 +1,26 @@
+import styles from './select_box.module.css';
+import React from 'react';
+
+interface ISelect {
+  value: string;
+  onChange: (value: string) => void;
+  optionList: { text: string; value: string }[];
+}
+
+const SelectBox = ({ value, onChange, optionList }: ISelect) => {
+  return (
+    <select
+      className={styles.select}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+    >
+      {optionList.map((option, idx) => (
+        <option key={idx} className={styles.option} value={option.value}>
+          {option.text}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default SelectBox;
