1028744fce8fbd1c6bab5eb5c9665740506ef8f3
Hihyu <4up456@naver.com>
2022-05-13 20:34:56 +0900

    button컴포넌트: 색상 타입 및 toggle기능 추가

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,32 @@
 import styles from './button.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-type TypeButton = { text: string; onClick: () => void };
-const Button = ({ text, onClick }: TypeButton) => {
+type TypeButton = {
+  text: string;
+  onClick: (event: React.MouseEvent<HTMLElement>) => void;
+  isToggle?: boolean;
+  typeReverse?: boolean;
+};
+const Button = ({
+  text,
+  onClick,
+  isToggle = false,
+  typeReverse = false,
+}: TypeButton) => {
+  const [isReverse, setIsReverse] = useState(typeReverse);
   const onSubmit = (event: React.MouseEvent<HTMLElement>) => {
-    onClick();
+    onClick(event);
+  };
+  const onSubmitAndToggle = (event: React.MouseEvent<HTMLElement>) => {
+    setIsReverse((prevIsReverse) => !prevIsReverse);
+    console.log(isReverse);
+    onClick(event);
   };
   return (
-    <button className={styles.btn} onClick={onSubmit}>
+    <button
+      className={isReverse ? `${styles.btn} ${styles.reverse}` : styles.btn}
+      onClick={isToggle ? onSubmitAndToggle : onSubmit}
+    >
       {text}
     </button>
   );
