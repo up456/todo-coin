4dc51e381087617215a4f9f88a08b313e23a157f
Hihyu <4up456@naver.com>
2022-05-17 17:10:53 +0900

    addTodoPage컴포넌트: ui완성

--- src/page/add_todo_page/add_todo_page.tsx
+++ src/page/add_todo_page/add_todo_page.tsx
@@ -1,6 +1,7 @@
 import styles from './add_todo_page.module.css';
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import Button from '../../components/button/button';
 
 const AddTodoPage = () => {
   const navigate = useNavigate();
@@ -12,6 +13,43 @@ const AddTodoPage = () => {
           <img src="/asset/arrow_left.png" alt="arrow_left" />
         </div>
         <h2 className={styles.headerTitle}>{`${date} To-do 작성`}</h2>
+      </section>
+      <form className={styles.addTodoBody}>
+        <div className={styles.inputBox}>
+          <label className={styles.inputLable}>할일</label>
+          <input type="text" className={styles.input} placeholder={'to-do'} />
+        </div>
+        <div className={styles.inputBox}>
+          <label className={styles.inputLable}>보상 코인</label>
+          <input
+            type="number"
+            className={styles.input}
+            placeholder={'reward-coin'}
+          />
+        </div>
+        <div className={styles.inputBox}>
+          <label className={styles.inputLable}>보상 경험치</label>
+          <input
+            type="number"
+            className={styles.input}
+            placeholder={'reward-exp'}
+          />
+        </div>
+        <div className={styles.inputBox}>
+          <label className={styles.inputLable}>카테고리</label>
+          <input
+            type="text"
+            className={styles.input}
+            placeholder={'category'}
+          />
+        </div>
+        <div className={styles.inputBox}>
+          <label className={styles.inputLable}>데드라인</label>
+          <input type="time" className={styles.input} />
+        </div>
+      </form>
+      <section className={styles.addTodoFooter}>
+        <Button text="작성하기" onClick={() => {}} />
       </section>
     </section>
   );
