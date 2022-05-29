b9bac479a9111433f6646f6b12c8c34901e02c97
Hihyu <4up456@naver.com>
2022-05-29 15:02:18 +0900

    필요 없는 부분 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
