e63688b7aeb637a2bd03da280ec986cf5c518671
Hihyu <4up456@naver.com>
2022-06-02 19:57:07 +0900

    상점 페이지: 토글btn ui 완성

--- src/components/item_card/item_card.tsx
+++ src/components/item_card/item_card.tsx
@@ -1,12 +1,46 @@
 import styles from './item_card.module.css';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Button from '../button/button';
 
 interface TypeItemCard {
   isPlus?: boolean;
 }
 
-const ItemCard = ({ isPlus = false }) => {
+const ItemCard = ({ isPlus = false }: TypeItemCard) => {
+  const otherBtnToggleRef = useRef<HTMLDivElement>(null);
+  const btnsContainerRef = useRef<HTMLDivElement>(null);
+  const [onFocus, setOnfocus] = useState(false);
+
+  const onClickOtherBtn = () => {
+    setOnfocus(!onFocus);
+  };
+  const outsideClick = (event: MouseEvent) => {
+    if (
+      !(
+        event.target === otherBtnToggleRef.current ||
+        event.target === otherBtnToggleRef.current?.firstElementChild ||
+        event.target === btnsContainerRef.current?.children[0] ||
+        event.target ===
+          btnsContainerRef.current?.children[0].firstElementChild ||
+        event.target ===
+          btnsContainerRef.current?.children[0].lastElementChild ||
+        event.target === btnsContainerRef.current?.children[1] ||
+        event.target ===
+          btnsContainerRef.current?.children[1].firstElementChild ||
+        event.target === btnsContainerRef.current?.children[1].lastElementChild
+      )
+    ) {
+      setOnfocus(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', outsideClick);
+    return () => {
+      window.removeEventListener('click', outsideClick);
+    };
+  }, []);
+
   return (
     <>
       {isPlus ? (
@@ -40,8 +74,38 @@ const ItemCard = ({ isPlus = false }) => {
                   text="구입하기"
                   onClick={() => {}}
                 />
-                <div className={styles.otherBtn}>
-                  <img src="/asset/other_btn.png" alt="기타 버튼" />
+                <div
+                  className={styles.otherBtnToggle}
+                  ref={otherBtnToggleRef}
+                  onClick={onClickOtherBtn}
+                >
+                  <img
+                    className={
+                      onFocus
+                        ? `${styles.otherBtnToggleImg} ${styles.onFocus}`
+                        : styles.otherBtnToggleImg
+                    }
+                    src={`/asset/${onFocus ? 'x' : 'other_btn'}.png`}
+                    alt="기타 버튼"
+                  />
+                </div>
+
+                <div
+                  className={
+                    onFocus
+                      ? styles.btnsContainer
+                      : `${styles.btnsContainer} ${styles.hidden}`
+                  }
+                  ref={btnsContainerRef}
+                >
+                  <div className={styles.btnBox}>
+                    <img src="/asset/update.png" alt="update" />
+                    <span>수정</span>
+                  </div>
+                  <div className={styles.btnBox}>
+                    <img src="/asset/delete.png" alt="delete" />
+                    <span>삭제</span>
+                  </div>
                 </div>
               </div>
             </div>
