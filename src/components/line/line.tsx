5581bf7c35512041eb96fca07cbf0ce3be9d0fd4
Hihyu <4up456@naver.com>
2022-06-04 20:12:19 +0900

    마이페이지: 프로필 & 현재 정보까지 완성

--- src/components/line/line.tsx
+++ src/components/line/line.tsx
@@ -3,15 +3,22 @@ import React from 'react';
 
 interface TypeLine {
   width?: string;
+  heigt?: string;
   mT?: string;
   mB?: string;
 }
-const Line = ({ width = '100%', mT = '1em', mB = '1em' }: TypeLine) => {
+const Line = ({
+  width = '100%',
+  heigt = '1.3px',
+  mT = '1em',
+  mB = '1em',
+}: TypeLine) => {
   return (
     <div
       className={styles.line}
       style={{
         width: width,
+        height: heigt,
         marginTop: mT,
         marginBottom: mB,
       }}
--- src/page/my_page/my_page.tsx
+++ src/page/my_page/my_page.tsx
@@ -1,10 +1,11 @@
 import styles from './my_page.module.css';
 import React from 'react';
-import Button from '../../components/button/button';
 import Header from '../../components/header/header';
 import { TypeData } from '../../App';
 import AuthService from '../../service/authService';
 import ItemCard from '../../components/item_card/item_card';
+import Tippy from '@tippyjs/react/headless';
+import Line from '../../components/line/line';
 
 interface TypeMyPage {
   data: TypeData;
@@ -30,8 +31,58 @@ const MyPage = ({
   return (
     <section className={styles.myPage}>
       <Header data={data} />
-      <h2>마이 페이지입니다.</h2>
-      <Button text="로그아웃" onClick={onLogout} />
+      <Tippy
+        render={(attrs) => (
+          <div className={styles.logoutTooltip} tabIndex={1} {...attrs}>
+            logout!
+          </div>
+        )}
+      >
+        <div className={styles.logoutBtn} onClick={onLogout}>
+          <img
+            className={styles.logoutBtnImg}
+            src="/asset/logout.svg"
+            alt="logout"
+          />
+        </div>
+      </Tippy>
+      <section className={styles.profileContainer}>
+        <div className={styles.nicknameBox}>
+          <h2 className={styles.nickname}>피카츄</h2>
+          <button
+            className={styles.nicknameEditBtn}
+          >{`닉네임 변경하기>`}</button>
+        </div>
+        <div className={styles.imgBox}>
+          <div
+            className={styles.img}
+            style={{ backgroundImage: `url('asset/default_profile.jpg')` }}
+          ></div>
+          <button className={styles.imgEditBtn}>
+            <img src="asset/profileImgEditBtn.svg" alt="" />
+          </button>
+        </div>
+      </section>
+      <section className={styles.nowInfoContainer}>
+        <div className={styles.nowInfoBox}>
+          <img src="asset/nowLv.svg" alt="현재Lv" />
+          <div className={`${styles.nowInfoKey} ${styles.firstKey}`}>
+            현재 Lv
+          </div>
+          <div className={styles.nowInfoValue}>
+            12 <b className={styles.keyText}>레벨</b>
+          </div>
+        </div>
+        <Line heigt="0.1px" width="80%" mT="0.5em" mB="0.5em" />
+        <div className={styles.nowInfoBox}>
+          <img src="asset/nowCoin.svg" alt="현재Coin" />
+          <div className={styles.nowInfoKey}>현재 Coin</div>
+          <div className={styles.nowInfoValue}>
+            20 <b className={styles.keyText}>코인</b>
+          </div>
+        </div>
+      </section>
+
       <h4>누적 todo:{data.total.totalTodo}</h4>
       <h4>누적 coin:{data.total.totalCoin}</h4>
       <h4>누적 item:{data.total.totalItem}</h4>
